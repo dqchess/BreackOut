44cff81a2e8b21e1581d22e097b88a3b75e1fe02
Ginnungagap <747994562@qq.com>
2019-03-25 00:39:10 +0800

    一些修改

--- Assets/GameEntrance1.cs
+++ Assets/GameEntrance1.cs
@@ -2,12 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 public class GameEntrance1 : MonoBehaviour
 {
+    /// <summary>
+    /// 显示弹窗的UI
+    /// </summary>
+    public GameObject UI;
+
+    /// <summary>
+    /// 小游戏描述文本
+    /// </summary>
+    public string UITextContent;
+
+    public string SceneName;
+
+    public Text UIText;
+
+    public Button ApplyButton;
+
+    public Button CancelButton;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -20,7 +40,27 @@ public class GameEntrance1 : MonoBehaviour
     {
         if(other.gameObject.tag=="Player")
         {
-            
+            UI.gameObject.SetActive(true);
+            UIText.text = UITextContent;
+            ApplyButton.onClick.RemoveAllListeners();
+            ApplyButton.onClick.AddListener(Click);
+            CancelButton.onClick.RemoveAllListeners();
+            CancelButton.onClick.AddListener(Cancel);
         }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        UI.gameObject.SetActive(false);
+    }
+
+    private void Click()
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+
+    private void Cancel()
+    {
+        UI.SetActive(false);
     }
 }
