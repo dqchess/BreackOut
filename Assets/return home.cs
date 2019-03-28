039657d1892819cbc35e66311b76007c3b597e02
yuanlee0715 <1059084529@qq.com>
2019-03-28 18:12:17 +0800

    改了一些界面，代码没改，你再帮我加上角色动画。谢谢啦

---
+++ Assets/return home.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class returnhome : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button btn = this.GetComponent<Button>();
+        btn.onClick.AddListener(OnClick);
+    }
+    void OnClick()
+    {
+        SceneManager.LoadScene("0");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
